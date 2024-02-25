2d93b89ce329a660840d7afade23d8f6df1b54ac
Jukakombo <56235252+Jukakombo@users.noreply.github.com>
2024-02-25 01:09:02 +0000

    Update AiContent.jsx

--- src/components/AiContent.jsx
+++ src/components/AiContent.jsx
@@ -4,7 +4,7 @@ import Text from "./Text";
 import InputQuestion from "./InputQuestion";
 // import Question from "./Question";
 import { useState } from "react";
-const API_KEY = "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";
+const API_KEY = "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";
 const systemMessage = {
   //  Explain things like you're talking to a software professional with 5 years of experience.
   role: "system",
